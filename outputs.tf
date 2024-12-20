f8ada8eb2f981bb4c59a3151536bc0361aafa41a
Alexander Kalaj <unixdisciple.us@gmail.com>
2024-12-20 14:33:00 -0800

    Removing JSON

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-# Generate JSON file containing aws_instance.minio_host disk names for first host
-resource "local_file" "disk_info" {
-  filename = "disk-info.json"
-  content  = jsonencode({
-    disks     = var.disk_names
-    size      = var.ebs_storage_volume_size
-    hostnames = var.minio_hosts
-  })
-}
